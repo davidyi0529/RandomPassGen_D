c36fad0ffc8408969b9f23c0a152a1ed0f8fc2e9
David Yi <david.yi.0529@gmail.com>
2020-09-19 00:08:35 -0700

    index.html updated

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
+  var length = prompt("Choose a length of at least 8 characters and no more than 128 characters","");
+  var lowercase = prompt("Would you like lowercase letters to be used? y/n","");
+  var uppercase = prompt("Would you like upcase letters to be used? y/n","");
+  var numeric = prompt("Would you like numeric numbers to be used? y/n","");
+  var special = prompt("Would you like special characters to be used? y/n","");
+  alert("Your password has been generated!!!");
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
